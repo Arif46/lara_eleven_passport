4d9454c77c8c33d2d4d3129f105a94637e1e97a4
Sulaiman <moktartee@gmail.com>
2024-05-21 00:08:42 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## passport install  
+
+## php artisan install:api --passport
+
+## composer require laravel/passport --ignore-platform-req=ext-sodium
+
+## php artisan passport:install
+
+## php artisan migrate
---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
+
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
+
+
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+
+Route::middleware('auth:api')->group( function () {
+    Route::resource('products', ProductController::class);
+});
